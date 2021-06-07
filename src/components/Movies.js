bfa18a5f6f96bd6e578ba6414f627be89593c345
jean <jin.ahn.1027@gmail.com>
2021-06-07 13:07:17 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,24 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => { 
+        return (
+          <div className={movie.title}>
+            {movie.title}<br/>
+            {movie.time}<br/>
+            {movie.metascore}<br/>
+            
+            <ul>
+            {movie.genres.map((genre)=>{
+              return (
+              <li>{genre}</li>
+              )
+            })}
+            </ul>
+          </div>
+      )
+      })}
     </div>
   );
 };
